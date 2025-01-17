b0f1db84dfdcf78a474ce7e3fab7bd1f1fa42ecb
gokhul <gokhul.tg@galvanizeme.com>
2025-01-17 21:02:33 +0530

    linked list, stack and queue problems

--- assignments/lecture-notes/day-3/TwoDArrays.java
+++ assignments/lecture-notes/day-3/TwoDArrays.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class TwoDArrays {
     public static void main(String[] args) {
-        // Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         // System.out.println("Enter the value of n : ");
         // int n = sc.nextInt();
         // System.out.println("Enter the value of m : ");
